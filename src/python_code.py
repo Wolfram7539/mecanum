81320ab63423ec99979a0abce0b3c11952f503f7
Wolfram7539 <fuyagon44@gmail.com>
2024-11-15 16:11:14 +0900

    feat: add function that convertment m/s to rpm

--- src/python_code.py
+++ src/python_code.py
@@ -18,18 +18,24 @@ class MECANUM():
         self.output = np.zeros(4)
     def __call__(self):
         print(self.r,self.H,self.L,self.output)
-    def calucurate(self,vx,vy,w):
+        
+    def calucurate(self,vx,vy,w,conv = False):
          self.output[0] = (1/self.r) * (vy-vx+(self.H+self.L)*w)
          self.output[1] = (1/self.r) * (vy+vx-(self.H+self.L)*w)
          self.output[2] = (1/self.r) * (vy+vx+(self.H+self.L)*w)
          self.output[3] = (1/self.r) * (vy-vx-(self.H+self.L)*w)
+         if conv:
+             self.output *= 60 / ( 2 * np.pi * self.r)
          return self.output
+     
     def vector_mecanum(self,vector,theta):
         self.theta = np.deg2rad(theta)
         self.rotateMat = np.array([
         [np.cos(theta), -np.sin(theta)],
         [np.sin(theta), np.cos(theta)],
     ])
+        
         self.nvector = np.dot(vector,self.rotateMat)
         print(self.nvector[0])
         return self.calucurate(self.nvector[0],self.nvector[1],0)
+    
